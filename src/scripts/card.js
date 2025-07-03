1e2bfd3b00eb4a79b6bc5ae86b5a59411c874c99
Dmitriy Balanovskiy <balanovskiy.05@icloud.com>
2025-07-03 14:48:01 +0300

    add validations form and connection server to project

--- src/scripts/card.js
+++ src/scripts/card.js
@@ -1,25 +1,53 @@
-export const createCard = (nameValue, linkValue, deleteCard, likeButtonState, showPicturePopup) => {
+import { deleteCardFromServer, likeCard, unlikeCard } from "./api";
+
+export const createCard = (nameValue, linkValue, deleteCard, likeButtonState, 
+  showPicturePopup, currentLikes, config, 
+  cardId, likesArray, ownerId, myId) => {
   const cardTemplate = document.querySelector("#card-template").content;
   const card = cardTemplate.querySelector(".card").cloneNode(true);
   const deleteButton = card.querySelector(".card__delete-button");
   const likeButton = card.querySelector(".card__like-button");
+  const likesCount = card.querySelector(".likes_counter");
   const cardImage = card.querySelector(".card__image");
   card.querySelector(".card__title").textContent = nameValue;
 
   cardImage.src = linkValue;
   cardImage.alt = `На фото: ${nameValue}`;
+  likesCount.textContent = currentLikes
 
-  deleteButton.addEventListener("click", () => deleteCard(card));
-  likeButton.addEventListener("click", () => likeButtonState(likeButton));
+  if (ownerId !== myId) {
+    deleteButton.remove();
+  }
+
+  if (likesArray && likesArray.some(user => user._id === myId)) {
+    likeButton.classList.add("card__like-button_is-active");
+  }
+
+  deleteButton.addEventListener("click", () => deleteCard(config, cardId, card));
+  likeButton.addEventListener("click", () => likeButtonState(likeButton, config, cardId, likesCount));
   cardImage.addEventListener("click", (evt) => showPicturePopup(evt, nameValue));
   
   return card;
 }
 
-export const deleteCard = (card) => { 
+export const deleteCard = (config, cardId, card) => { 
+  deleteCardFromServer(config, cardId);
   card.remove();
 }
 
-export const likeButtonState = (target) => {
-  target.classList.toggle("card__like-button_is-active");
+export const likeButtonState = (target, config, cardId, likesCount) => {
+  likeCard(config, cardId)
+  .then(data => {
+    target.classList.add("card__like-button_is-active");
+    likesCount.textContent = data.likes.length;
+  })
+  unlikeCard(config, cardId)
+  .then(data => {
+    target.classList.remove("card__like-button_is-active");
+    if (likesCount.textContent === "1") { 
+      likesCount.textContent = "";
+    } else { 
+      likesCount.textContent = data.likes.length;
+    }
+  })
 }
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,47 +1,101 @@
-import '../index.css';
-import {initialCards} from './cards.js';
+import "../index.css";
 import {createCard, deleteCard, likeButtonState} from "./card.js";
 import {showModal, closeModal} from "./modal.js";
+import {enableValidation, clearValidation} from "./validation.js"
+import {getProfileInfo, getCards, updateProfileInfo, addNewCardOnServer, updateAvatar} from "./api.js"
 
 const cardList = document.querySelector(".places__list");
-const profileTile = document.querySelector(".profile__title");
+const profileTitle = document.querySelector(".profile__title");
 const profileDesciption = document.querySelector(".profile__description");
+const profileAvatar = document.querySelector(".profile__image")
 const addCardForm = document.forms.new_place;
 const placeName = addCardForm.elements.place_name;
 const placeLink = addCardForm.elements.link;
+const addCardFormSubmitButton = addCardForm.elements.new_place_form_submit_button;
+const updateAvatarForm = document.forms.new_avatar;
+const updateAvatarSubmitButton = updateAvatarForm.elements.new_avatar_form_submit_button
+const placeLinkAvatar = updateAvatarForm.elements.new_avatar_link;
 const editProfileButton = document.querySelector(".profile__edit-button");
 const addCardButton = document.querySelector(".profile__add-button")
 const editProfileForm = document.forms.edit_profile;
 const personName = editProfileForm.elements.name;
 const personDescription = editProfileForm.elements.description;
+const editProfileFormSubmitButton = editProfileForm.elements.edit_profile_submit_button;
 const newCardModal = document.querySelector(".popup_type_new-card");
 const editProfileModal = document.querySelector(".popup_type_edit");
+const updateAvatarModal = document.querySelector(".popup_type_update-avatar");
 const popupTypeImage = document.querySelector(".popup_type_image");
 const imgShowPopup = document.querySelector(".popup__image")
 const imgDescriptionShowPopup = document.querySelector(".popup__caption");
+const updateAvatarButton = document.querySelector(".profile__image")
+let myId = null;
+const config = {
+  baseUrl: 'https://nomoreparties.co/v1/wff-cohort-41',
+  headers: {
+    authorization: "b2069597-819f-48e3-8235-452d763c5957",
+    'Content-Type': 'application/json'
+  }
+}
+const validationConfig = {
+  formSelector: ".popup__form",
+  inputSelector: ".popup__input",
+  submitButtonSelector: ".popup__button",
+  inactiveButtonClass: "button_inactive",
+  inputErrorClass: "form__input_type_error",
+  errorClass: "form__input-error_active"
+};
 
 const addCard = (card, cardList) => { 
   cardList.append(card);
 }
 
-const editInputEditProfile = () => { 
-  personName.value = profileTile.textContent;
+const editInputEditProfile = () => {
+  clearValidation(editProfileModal, validationConfig);
+  personName.value = profileTitle.textContent;
   personDescription.value = profileDesciption.textContent;
 }
 
-const EditProfileFormSubmit = (evt) => { 
+const editProfileFormSubmit = (evt) => { 
   evt.preventDefault();
-  profileTile.textContent = personName.value;
-  profileDesciption.textContent = personDescription.value;
-  closeModal(editProfileModal);
-}
+  editProfileFormSubmitButton.textContent = "Сохранение...";
+  updateProfileInfo(config, personName.value, personDescription.value)
+    .then((data) => {
+      profileTitle.textContent = data.name;
+      profileDesciption.textContent = data.about;
+      closeModal(editProfileModal);
+      editProfileFormSubmitButton.textContent = "Сохранить";
+    })
+};
+
 
 const addCardFormSubmit = (evt) => { 
   evt.preventDefault();
-  const newCard = createCard(placeName.value, placeLink.value, deleteCard, likeButtonState, showPicturePopup);
-  cardList.prepend(newCard);
-  addCardForm.reset();
-  closeModal(newCardModal);
+  addCardFormSubmitButton.textContent = "Сохранение...";
+  addNewCardOnServer(config, placeName.value, placeLink.value)
+    .then((cardInfoObject) => {
+      const card = createCard(cardInfoObject.name, cardInfoObject.link, 
+        deleteCard, likeButtonState, showPicturePopup, 
+        showLikes(cardInfoObject.likes.length), 
+        config, cardInfoObject._id, cardInfoObject.likes, 
+        cardInfoObject.owner._id, myId);
+      cardList.prepend(card);
+      clearValidation(newCardModal, validationConfig);
+      closeModal(newCardModal);
+      addCardForm.reset();
+      addCardFormSubmitButton.textContent = "Сохранить";
+    })
+}
+
+const updateAvatarFormSubmit = (evt) => { 
+  evt.preventDefault();
+  updateAvatarSubmitButton.textContent = "Сохранение...";
+  updateAvatar(config, placeLinkAvatar.value)
+  .then((res) => { 
+    profileAvatar.style.backgroundImage = `url(${res.avatar})`;  
+      closeModal(updateAvatarModal);
+      updateAvatarForm.reset(); 
+      updateAvatarSubmitButton.textContent = "Сохранить";
+  })
 }
 
 const showPicturePopup = (evt, nameValue) => { 
@@ -50,11 +104,6 @@ const showPicturePopup = (evt, nameValue) => {
   imgDescriptionShowPopup.textContent = nameValue;
   showModal(popupTypeImage);
 }
-
-initialCards.forEach((elem) => {
-  const card = createCard(elem.name, elem.link, deleteCard, likeButtonState, showPicturePopup);
-  addCard(card, cardList);
-})
 
 editProfileButton.addEventListener("click", () => { 
   editInputEditProfile();
@@ -65,7 +114,29 @@ addCardButton.addEventListener("click", () => {
   showModal(newCardModal);
 })
 
-editProfileForm.addEventListener("submit", EditProfileFormSubmit);
+updateAvatarButton.addEventListener("click", () =>{
+  showModal(updateAvatarModal);
+})
 
-addCardForm.addEventListener("submit", addCardFormSubmit)
+editProfileForm.addEventListener("submit", editProfileFormSubmit);
 
+addCardForm.addEventListener("submit", addCardFormSubmit);
+
+updateAvatarForm.addEventListener("submit", updateAvatarFormSubmit)
+
+enableValidation(validationConfig);
+
+const showLikes = (currentLikes) => currentLikes || "";
+
+Promise.all([getProfileInfo(config, profileTitle, profileDesciption, 
+  profileAvatar), getCards(config)])
+.then(([userData, cards]) => {
+  myId = userData._id;
+  cards.forEach((cardInfoObject) => {
+    const card = createCard(cardInfoObject.name, cardInfoObject.link, 
+      deleteCard, likeButtonState, showPicturePopup, 
+      showLikes(cardInfoObject.likes.length), config, 
+      cardInfoObject._id, cardInfoObject.likes, cardInfoObject.owner._id, myId);
+      addCard(card, cardList);
+    }) 
+})
